9461fe258d7978e6fcab6262e959ce02abf935f8
zarag <zarag@DESKTOP-TGO2GI2>
2022-02-28 12:26:26 +0100

    Hola

---
+++ src/entornos/Entornos.java
@@ -0,0 +1,17 @@
+
+package entornos;
+
+/**
+ *
+ * @author zarag
+ */
+public class Entornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
