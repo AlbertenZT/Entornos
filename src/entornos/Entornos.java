76cfb4c1503865962950d1135d107b1005a7a554
rafa <rafa@DESKTOP-SUVCMUG>
2022-02-28 12:45:52 +0100

    Scanner añadido

--- src/entornos/Entornos.java
+++ src/entornos/Entornos.java
@@ -1,6 +1,8 @@
 
 package entornos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author zarag
@@ -11,8 +13,13 @@ public class Entornos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hola");
         System.out.println("hola2");
+        
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+        System.out.println("Hola "+nombre);
     }
     
 }
